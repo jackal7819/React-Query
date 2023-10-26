be106fe6a7c1ff5e61aff26ba301fdef0b235b06
Viktor Filippov <jackal3166@gmail.com>
2023-10-27 02:41:44 +0300

    Refactoring

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import { useCreateTask } from './tanstackCustomHooks';
+import { useState } from 'react';
 
 const Form = () => {
 	const { isLoading, createTask } = useCreateTask();
